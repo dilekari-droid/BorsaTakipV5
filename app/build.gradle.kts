b5cb87ee274ad4b99e82041b54b602fe20ed3c40
dilekari-droid <dilekari@gmail.com>
2026-09-09 13:44:09 +0300

    Bump version to 5.1.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "tr.borsatakip.v5"
         minSdk = 26
         targetSdk = 35
-        versionCode = 58
-        versionName = "5.1.8"
+        versionCode = 59
+        versionName = "5.1.9"
     }
 
     val releaseStorePath = System.getenv("BORSA_KEYSTORE_PATH")
