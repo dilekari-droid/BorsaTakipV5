99675a2b20a8a5e0571e6d4cf6772b03da95b660
dilekari-droid <dilekari@gmail.com>
2026-09-11 22:58:27 +0300

    V5.1.30 sürüm numarasını artır

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "tr.borsatakip.v5"
         minSdk = 26
         targetSdk = 35
-        versionCode = 79
-        versionName = "5.1.29"
+        versionCode = 80
+        versionName = "5.1.30"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
